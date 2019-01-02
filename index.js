c0316b40acafacc1d480827d58fd6588046bcd01
Roberto Souza <robertos.cps@gmail.com>
2019-01-02 05:19:45 -0200

    Second fix for AWS teste

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ consign()
     .then('./src/config/routes.js')  
     .into(app)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 9000
 app.listen(port, () => {
     console.log(`Servidor funcionando na porta ${port}`)
 })
