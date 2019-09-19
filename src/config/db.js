e80f6e0413886091b7f81c2f7937f0d91005ce0a
Roberto Souza <robertos.cps@gmail.com>
2019-09-19 13:44:32 -0300

    Update

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,13 @@
 
 const mongoose = require('mongoose')
 mongoose.set('useCreateIndex', true)
+mongoose.set('useNewUrlParser', true)
+mongoose.set('useUnifiedTopology', true)
 mongoose.set('useFindAndModify', false)
 
 module.exports = {
     openConn() {
-        mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true }).catch(_ => {
+        mongoose.connect(process.env.DB_HOST).catch(_ => {
             console.log('\x1b[41m\x1b[37m', 'ERRO AO SE CONECTAR COM O BANCO DE DADOS!', '\x1b[0m')
         })
         require('../model/userModel')
